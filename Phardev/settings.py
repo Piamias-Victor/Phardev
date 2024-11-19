f116c13ff30a7577384cf277a8be36977cd5af50
MysteriousBaboon <46499985+MysteriousBaboon@users.noreply.github.com>
2024-11-20 00:08:39 +0100

    Update settings.py

--- Phardev/settings.py
+++ Phardev/settings.py
@@ -63,7 +63,7 @@ else:
         },
     }
 
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True

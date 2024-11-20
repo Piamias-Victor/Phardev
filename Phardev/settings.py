175d07363574bb3a504f3af30f6c11efca32d77f
MysteriousBaboon <46499985+MysteriousBaboon@users.noreply.github.com>
2024-11-20 21:36:25 +0100

    Fix https

--- Phardev/settings.py
+++ Phardev/settings.py
@@ -64,7 +64,8 @@ else:
 
     #TODO put in true when domain is parametered
     SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
 
@@ -91,7 +92,7 @@ else:
     }
 
 # Allowed hosts and CORS settings
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['api.phardev.fr', 'localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     'api.phardev.fr'
 ]
